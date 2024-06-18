54a5b7134343c54603d0be74accc1af2a8e4d992
h4rel <shouswim@icloud.com>
2024-06-18 22:44:59 +0900

    test

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float x, y;
+    float speed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 vec = new Vector3(x, y);
+        transform.position += vec.normalized * speed;
+    }
+}
