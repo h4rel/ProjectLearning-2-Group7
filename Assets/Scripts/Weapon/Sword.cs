194347869b1819cd09f5f22808894c78298233eb
liu-jie-903 <147288297+liu-jie-903@users.noreply.github.com>
2024-06-29 18:02:45 +0900

    Add a sword to the player(Click the Left Botton for Mouse)

---
+++ Assets/Scripts/Weapon/Sword.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+        activeWeapon = GetComponent<ActiveWeapon>();
+        myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Start()
+    {
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Attack()
+    {
+        myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoiny = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if(mousePos.x < playerScreenPoiny.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+}
