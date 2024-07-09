cd15037f6d5883d3ff62ef1d56dd2f2febc4e8ce
liu-jie-903 <147288297+liu-jie-903@users.noreply.github.com>
2024-07-09 13:43:08 +0900

    Errors

--- Assets/Scripts/Enemy/Shooter.cs
+++ Assets/Scripts/Enemy/Shooter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
 public class Shooter : MonoBehaviour,IEnemy
 {
     [SerializeField] private GameObject bulletPrefab;
@@ -12,5 +13,48 @@ public class Shooter : MonoBehaviour,IEnemy
 
         GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
         newBullet.transform.right = targetDirection;
+    }
+}
+*/
+
+public class Shooter : MonoBehaviour ,IEnemy
+{
+    [SerializeField] private GameObject bulletPrefabs;
+    [SerializeField] private float bulletMoveSpeed;
+    [SerializeField] private int burstCount;
+    [SerializeField] private float timeBetweenBursts;
+    [SerializeField] private float restTime = 1f;
+
+    private bool isShooting = false;
+
+    public void Attack()
+    {
+        if (!isShooting)
+        {
+            StartCoroutine(ShootRoutine());
+        }
+    }
+
+    private IEnumerator ShootRoutine()
+    {
+        isShooting = true;
+
+        for (int i = 0; i < burstCount; i++)
+        {
+            Vector2 targetDirection = PlayerControllers.Instance.transform.position - transform.position;
+
+            GameObject newBullet = Instantiate(bulletPrefabs, transform.position, Quaternion.identity);
+            newBullet.transform.right = targetDirection;
+
+            if (newBullet.TryGetComponent(out Projectile projectile))
+            {
+                projectile.UpdateMoveSpeed(bulletMoveSpeed);
+            }
+
+            yield return new WaitForSeconds(timeBetweenBursts);
+        }
+
+        yield return new WaitForSeconds(restTime);
+        isShooting = false;
     }
 }
