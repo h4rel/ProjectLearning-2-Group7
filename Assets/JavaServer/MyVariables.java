231448156bb1248011d8845af5dad3b75c5629b5
Yuki Nishioka <2003yuu05@gmail.com>
2024-07-22 20:35:47 +0900

    ランキングで使用したjavaのコード載せておきます。

---
+++ Assets/JavaServer/MyVariables.java
@@ -0,0 +1,7 @@
+public class MyVariables {
+    static final String SERVER_IPADDR = "192.168.3.152";
+    static final int PORT = 5000;
+    static final String DB_URL = "jdbc:mysql://localhost:3306/projectlearning-2-group7";
+    static final String DB_USERNAME = "root";
+    static final String DB_PASSWORD = "sql0522";
+}
